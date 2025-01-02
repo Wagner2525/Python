0f968537cf2e18bad4d0215a1fcccc3951bb099a
Wagner Lucas <Wagnerlucas387@gmail.com>
2025-01-02 15:06:58 -0300

    Update Ex021(MP3).py

--- Curso de Python/Python 3 - Mundo 1/Ex021(MP3).py
+++ Curso de Python/Python 3 - Mundo 1/Ex021(MP3).py
@@ -2,7 +2,7 @@ import pygame
 #iniciar
 pygame.init()
 #pegar a musica
-pygame.mixer.music.load(r"C:\Users\wagne\Documents\Repositorio - Curso em Vd\Python\Curso de Python\Python 3 - Mundo 1\LogoEu.mp3")
+pygame.mixer.music.load(r"C:\Users\wagne\Documents\Repositorios\Python\Curso de Python\Python 3 - Mundo 1\LogoEu.mp3")
 #pegar a musica
 pygame.mixer.music.play()
 input("Pressione ENTER para sair...") 
