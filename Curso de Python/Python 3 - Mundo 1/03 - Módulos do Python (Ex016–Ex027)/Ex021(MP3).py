bc25d4bc3f7b86daa735285f4b6b6fc13f7871fb
Wagner Lucas <Wagnerlucas387@gmail.com>
2025-07-31 13:17:50 -0300

    Organiza estrutura de pastas e exercícios

--- Curso de Python/Python 3 - Mundo 1/03 - Módulos do Python (Ex016–Ex027)/Ex021(MP3).py
+++ Curso de Python/Python 3 - Mundo 1/03 - Módulos do Python (Ex016–Ex027)/Ex021(MP3).py
@@ -2,7 +2,7 @@ import pygame
 #iniciar
 pygame.init()
 #pegar a musica
-pygame.mixer.music.load(r"C:\Users\wagne\Documents\Repositorios\Python\Curso de Python\Python 3 - Mundo 1\LogoEu.mp3")
+pygame.mixer.music.load(r"C:\Users\wagne\Documents\Repositorios\Python\Curso de Python\Python 3 - Mundo 1\03 - Módulos do Python (Ex016–Ex027)\LogoEu.mp3")
 #pegar a musica
 pygame.mixer.music.play()
 input("Pressione ENTER para sair...") 
